aaa13cf1380fd68159b7b4a8d3f79ba079b2dd60
CAT9527 <37021526+CAT9527@users.noreply.github.com>
2019-01-17 20:43:49 +0800

    1

---
+++ 104_数据结构CSharp版_二叉树/LinkBiSearchTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _104_数据结构CSharp版_二叉树
+{
+    /// <summary>
+    /// 在二叉树中，如果一个结点的左子结点永远小于该结点的值，右子结点永远大于该结点的值，这样的二叉树叫做二叉搜索树
+    /// 用“二叉搜索树”解决快速搜索磁盘文件记录的问题
+    /// </summary>
+    
+    public struct indexNode
+    {
+        private int key;
+        private int offset;
+        public indexNode(int key,int offset)
+        {
+            this.key = key;
+            this.offset = offset;
+        }
+    }
+    class LinkBiSearchTree
+    {
+    }
+}
--- 104_数据结构Csharp版_数组/GuessNumber.cs
+++ 104_数据结构Csharp版_数组/GuessNumber.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
+/// <summary>
+/// 数学魔术游戏（有错误）
+/// </summary>
 namespace _104_数据结构Csharp版_数组
 {
     class GuessNumber
@@ -13,10 +16,59 @@ namespace _104_数据结构Csharp版_数组
             int[,] num = new int[5, 6];           //存储卡片上的数字
             int[] len = new int[5];               //动态添加卡片上的数字，统计每张卡片上数字的个数
             char[] bit;
-            for (int i = 0; i <=31; i++)
+            for (int i = 1; i <= 31; i++)
             {
-                bit = t
+                bit = ToBinary(i);
+                for (int j = 0; j < bit.Length; j++)
+                    if (bit[j] == '1')
+                        num[j, len[j]++] = i;
             }
+
+            //随机产生一个1到31之间的整数
+            Random ra = new Random(unchecked((int)DateTime.Now.Ticks));
+            int numrand = ra.Next(1, 31);
+            bit = ToBinary(numrand);
+            string cardrand = "";
+            for (int i = 0; i < bit.Length; i++)
+                if (bit[i] == '1')
+                    cardrand = cardrand + (i + 1) + ",";
+            Console.Write("在卡片：{0}都存在的数是：", cardrand);
+            int guessnum = Convert.ToInt32(Console.ReadLine());
+            if (guessnum == numrand)
+                Console.WriteLine("恭喜你，猜对了");
+            else
+                Console.WriteLine("猜错了，该数应该是{0}，再试一次！", numrand);
+
+            //列出随即数据在卡片上的数字
+            Console.WriteLine("{0}在卡片上的数字为：", numrand);
+            for (int i = 0; i < bit.Length; i++)
+            {
+                if (bit[i] == '1')
+                {
+                    Console.Write("卡片{0}：", (i + 1));
+                    for (int j = 0; j < 16; j++)
+                    {
+                        Console.WriteLine(num[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        //将一个十进制数转换成二进制数并将其存放在字符数组中
+        public static char[] ToBinary(int x)
+        {
+            char temp;               //实现bit反转时用
+            char[] bit = new char[5];
+            bit = Convert.ToString(x, 2).ToCharArray();
+            for (int j = 0; j < bit.Length / 2; j++)
+            {
+                temp = bit[j];
+                bit[j] = bit[bit.Length - 1 - j];
+                bit[bit.Length - 1 - j] = temp;
+            }
+            return bit;
         }
     }
 }
